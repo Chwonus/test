a5d326664a880c05a2dd2779d1dd0004394ff0b8
Chwonus <uconconu@gmail.com>
2024-11-20 09:07:07 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="jquery-3.7.1.min.js"></script>
     <title>ESP32-CAM Captured Photo Gallery</title>
   </head>
   <body style="background-color:#202125;" id="myESP32CAMPhotos">
